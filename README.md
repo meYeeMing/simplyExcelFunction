13ef040097a6103faf56669e54e934121085bd7f
meYeeMing <yee.ming@hotmail.com>
2026-01-30 14:33:39 +0800

    first function include

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # simplyExcelFunction
+
+Description : Personal Excel function collection that support me work faster in excel 
+
+## Getting Started 
+Build the package and copy the 64bit `.xll` from `bin\Debug\net10.0-windows\publish\SimpleExcelfunctions-AddIns64.xll` to your folder in your PC. 
+
+## Add into Excel 
+1. Open Excel
+2. Open the Options from the File menu item 
+3. in the Add-ins page , find Manage = Excel Add-ins. 
+<div style="text-align: center;">
+  <img src="readme\1.png" alt="Centered image">
+</div>
+4. Click Go 
+5. use Browse to add your `.xll` into your excel. 
+6. Restart the Excel
+
+## Current Function 
+**`=reDateTime($cell)`** use to convert a string date time format(yyyy-mm-dd hh:mm:ss) to excel date time format. 
---
+++ SimplyExcelFunctions/DateTimeFunction.cs
@@ -0,0 +1,26 @@
+﻿namespace SimplyExcelFunctions;
+
+using ExcelDna.Integration;
+using System;
+using System.Globalization;
+using Microsoft.VisualBasic;
+
+public class DateTimeFunction
+{
+    [ExcelFunction(Description = "reformat to DateTime From String yyyy-mm-dd hh:mm:ss", IsVolatile = true, Category = "DateTime Functions")]
+    public static object reDateTime(string input)
+    {
+        if (string.IsNullOrEmpty(input) || input.Length < 19)
+            return "Invalid Input";
+
+        try
+        {
+            DateTime dt = DateTime.ParseExact(input.Substring(0, 19), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return dt;
+        }
+        catch (Exception)
+        {
+            return ExcelError.ExcelErrorValue;
+        }
+    }
+}
